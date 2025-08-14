565c2519b31354e714a06695748d1c51c9a9cedf
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-08-14 18:37:08 +0300

    Add exception filters to handle exceptions globally

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './exception-filters/all-exceptions.filter';
+import { UnauthorizedExceptionFilter } from './exception-filters/unauthorized-http-exception.filter';
+import { PrismaClientExceptionFilter } from './exception-filters/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  const httpAdapterHost = app.get(HttpAdapterHost);
 
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+  app.useGlobalFilters(new UnauthorizedExceptionFilter());
+  app.useGlobalFilters(new PrismaClientExceptionFilter());
+
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
