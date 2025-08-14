18ae994f7ee18dada11494313755c8d338d2eede
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-08-14 19:39:11 +0300

    feat: Add HttpLoggingInterceptor to log HTTP requests and responses

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,25 @@ import { BooksModule } from './books/books.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpLoggingInterceptor } from './interceptors/http-logging.interceptor';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      expandVariables: true,
+      expandVariables: false,
     }),
     BooksModule,
     DatabaseModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: HttpLoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
