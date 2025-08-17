73ecd3dfbf59d86e8fd270b004573a7a4c08369a
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-08-17 14:54:23 +0300

    docs: Add Swagger for API documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,18 +11,34 @@ import { AuthPayloadDto } from './dto/auth-payload.dto';
 import { AuthService } from './auth.service';
 import { LocalGuard } from './guards/local.guard';
 import type { Request } from 'express';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Authentication')
+@ApiBadRequestResponse({
+  description: 'When request has invalid parameter/request body',
+})
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('login')
   @HttpCode(200)
+  @ApiBody({ type: AuthPayloadDto, description: 'User logging in' })
+  @ApiOkResponse({ description: 'User logged in successfully' })
+  @ApiUnauthorizedResponse({ description: 'Invalid User Credentials' })
   @UseGuards(LocalGuard)
   async login(@Req() req: Request) {
     return { token: await this.authService.generateToken(req.user!.id) };
   }
 
   @Post('register')
+  @ApiCreatedResponse({ description: 'User registered successfully' })
   async register(@Body(ValidationPipe) newUser: AuthPayloadDto) {
     return await this.authService.createUser(newUser);
   }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './exception-filters/all-exceptions.filter';
 import { UnauthorizedExceptionFilter } from './exception-filters/unauthorized-http-exception.filter';
 import { PrismaClientExceptionFilter } from './exception-filters/prisma-client-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,15 @@ async function bootstrap() {
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapterHost));
 
   app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Bookshelf API')
+    .setDescription('Book management API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
